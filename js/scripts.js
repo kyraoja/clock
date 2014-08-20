7dd87cae28accf3faf945b0ecef5ce57733183a4
kyraoja <koja1@EWU-Rename-7.local>
2014-08-20 15:08:47 -0700

    background for different times

--- js/scripts.js
+++ js/scripts.js
@@ -112,26 +112,20 @@ $(document).ready(function(){
 
 /*Day/Night*/
 
-$(document).ready(function() {
+$(document).ready(function(){
+  var d = new Date();
+  var n = d.getHours();
 
-  $('#day').on('click', function() {
-  
-     //go day
-  $('body').addClass('day').removeClass('night');
-    
-   });
-  
-  
-  $('#night').on('click', function() {
-    
-    //go night
-  $('body').addClass('night').removeClass('day');
-  
+  if (n > 19 || n < 6)
+    // If time is after 7PM or before 6AM, apply night theme to ‘body’
+    document.body.className = "night";
+  else if (n > 16 && n < 19)
+    // If time is between 4PM – 7PM sunset theme to ‘body’
+    document.body.className = "sunset";
+  else
+    // Else use ‘day’ theme
+    document.body.className = "day";
 });
-
-});
-
-
 
 
 
